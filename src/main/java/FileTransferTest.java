89c6316efab611c381c78bea6df243486ac46cd3
ronCAutomation <rahul.chaudhary333@gmail.com>
2021-09-12 11:07:55 +0530

    Initial commit

---
+++ src/main/java/FileTransferTest.java
@@ -0,0 +1,8 @@
+import com.jcraft.jsch.Session;
+
+import javax.enterprise.inject.spi.SessionBeanType;
+
+public class FileTransferTest {
+
+    Session on = null;
+}
---
+++ src/main/java/MyTestClass.java
@@ -0,0 +1,44 @@
+import org.checkerframework.checker.units.UnitsTools;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class MyTestClass {
+
+    static Path source = Paths.get("D:\\Work\\SampleDirectorySource\\JingName.txt");
+    static Path destination = Paths.get("D:\\Work\\SampleDirectoryTarget");
+    static Path renamedSource;
+
+    public static Path renameFile(Path source)
+    {
+        try{
+            System.out.println(String.valueOf(source));
+            renamedSource = source.resolveSibling("MingName.txt");
+            Files.move(source, renamedSource);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+        return renamedSource;
+
+    }
+
+    public static void moveFileToOtherDirectory(Path source, Path destination) throws IOException
+    {
+        Files.createDirectories(destination);
+        Files.move(source, destination.resolve(source.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+    //    moveFileToOtherDirectory(renameFile(source), destination);
+        System.out.println("Hello friend");
+
+    }
+}
